8664cf7a63f44cc4a86467264035988c1cc07237
IvanKazakov2352 <ivankazakov3344@gmail.com>
2024-02-11 11:22:32 +0300

    chore(app.vue): add SEO meta headings

--- app.vue
+++ app.vue
@@ -6,9 +6,18 @@
 </template>
 <script setup lang="ts">
 useSeoMeta({
+  applicationName: 'Personal website of Ivan Kazakov',
+  author: 'Ivan Kazakov',
   title: 'Ivan Kazakov',
   ogTitle: 'Ivan Kazakov',
   description: 'Personal website of developer and product manager Ivan Kazakov! It contains all information about my activity and portfolio!',
   ogDescription: 'Personal website of developer and product manager Ivan Kazakov! It contains all information about my activity and portfolio!',
+  keywords: 'site, Ivan Kazakov, personal, software, programming, resume',
+  ogUrl: 'https://ikazakov.com/',
+  ogSiteName: 'Ivan Kazakov',
+  ogLocale: 'ru_RU',
+  ogType: 'website',
+  twitterTitle: 'Ivan Kazakov',
+  twitterDescription: 'Personal website of developer and product manager Ivan Kazakov! It contains all information about my activity and portfolio!'
 })
 </script>
