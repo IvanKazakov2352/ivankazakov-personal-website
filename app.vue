96f0cbe8d7881aa38661173646dcedae095e2bf9
IvanKazakov2352 <ivankazakov3344@gmail.com>
2024-02-22 22:58:59 +0300

    chore(app.vue): update seo tags

--- app.vue
+++ app.vue
@@ -10,14 +10,25 @@ useSeoMeta({
   author: 'Ivan Kazakov',
   title: 'Ivan Kazakov',
   ogTitle: 'Ivan Kazakov',
-  description: 'Personal website of developer and product manager Ivan Kazakov! It contains all information about my activity and portfolio!',
-  ogDescription: 'Personal website of developer and product manager Ivan Kazakov! It contains all information about my activity and portfolio!',
+  description: 'Personal website of developer and product manager Ivan Kazakov!',
+  ogDescription: 'Personal website of developer and product manager Ivan Kazakov!',
+  ogImage: "https://objects.ikazakov.com/main.jpg",
   keywords: 'site, Ivan Kazakov, personal, software, programming, resume',
   ogUrl: 'https://ikazakov.com/',
   ogSiteName: 'Ivan Kazakov',
+  ogImageWidth: '944',
+  ogImageHeight: '630',
   ogLocale: 'ru_RU',
   ogType: 'website',
   twitterTitle: 'Ivan Kazakov',
-  twitterDescription: 'Personal website of developer and product manager Ivan Kazakov! It contains all information about my activity and portfolio!'
+  twitterDescription: 'Personal website of developer and product manager Ivan Kazakov!',
+  twitterImage: "https://objects.ikazakov.com/main.jpg",
+  twitterImageAlt: 'Main image',
+  charset: 'utf-8',
+  colorScheme: 'only light',
+  rating: 'adult',
+  googlebotNews: 'noindex',
+  googlebot: 'noarchive',
+  google: 'nopagereadaloud'
 })
 </script>
