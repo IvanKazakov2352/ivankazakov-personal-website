488fed65ec4e66225254cb5a69e0e059c8ce1e40
IvanKazakov2352 <ivankazakov3344@gmail.com>
2024-06-23 19:16:49 +0300

    chore(app.vue): update seo tags

--- app.vue
+++ app.vue
@@ -6,15 +6,15 @@
 </template>
 <script setup lang="ts">
 useSeoMeta({
-  applicationName: 'Personal website of Ivan Kazakov',
   author: 'Ivan Kazakov',
   title: 'Ivan Kazakov',
   creator: 'Ivan Kazakov',
   ogTitle: 'Ivan Kazakov',
-  description: 'Personal website of developer Ivan Kazakov!',
-  ogDescription: 'Personal website of developer Ivan Kazakov!',
+  applicationName: 'Персональный веб-сайт Ивана Казакова',
+  description: 'Персональный веб-сайт разработчика и начинающего продукт менеджера - Ивана Казакова!',
+  ogDescription: 'Персональный веб-сайт разработчика и начинающего продукт менеджера - Ивана Казакова!',
   ogImage: "https://objects.ikazakov.com/main.jpg",
-  keywords: 'site, Ivan Kazakov, personal, software, programming, resume',
+  keywords: 'site, Ivan Kazakov, personal, software, programming, resume, freelance-finder, Иван Казаков',
   ogUrl: 'https://ikazakov.com/',
   ogSiteName: 'Ivan Kazakov',
   ogImageWidth: '944',
@@ -22,12 +22,14 @@ useSeoMeta({
   ogLocale: 'ru_RU',
   ogType: 'website',
   twitterTitle: 'Ivan Kazakov',
-  twitterDescription: 'Personal website of developer Ivan Kazakov!',
+  twitterDescription: 'Персональный веб-сайт разработчика и начинающего продукт менеджера - Ивана Казакова!',
   twitterImage: "https://objects.ikazakov.com/main.jpg",
   twitterImageAlt: 'Main image',
   charset: 'utf-8',
   colorScheme: 'only light',
-  robots: 'index, follow'
+  robots: 'index, follow',
+  rating: 'adult',
+  formatDetection: 'telephone=no'
 })
 useHead({
   meta: [
